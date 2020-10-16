b6327ed654ba375105da23b7a6108c5b35f4909c
Leao04 <lordvergil12345@hotmail.com>
2020-10-15 22:13:37 -0300

    ..

---
+++ .nuxt/components/index.js
@@ -0,0 +1,4 @@
+
+xport { default as Header } from '../..\\components\\Header.vue'
+
+export const LazyHeader = import('../..\\components\\Header.vue' /* webpackChunkName: "components_Header" */).then(c => c.default || c)
